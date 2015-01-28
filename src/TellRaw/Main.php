a33e2267eb0941f4c045c2c8ad42d02c6c006cd5
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-01-27 21:28:02 -0500

    Update Main.php for new commands (PLEASE HELP)

--- src/TellRaw/Main.php
+++ src/TellRaw/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TellRaw;
+namespace iRaw;
 
 /*
  * 
@@ -23,25 +23,41 @@ class Main extends PluginBase implements Listener {
 		
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
 		if($sender instanceof Player) {
-			$player = $args->getPlayer()->getName();
+			$player = $args[1]->getPlayer()->getName();
+			$message = $args[2]->getMessage();
 			if(strtolower($command->getName('raw'))) {
-				// CREATE
-				if(empty($args)) {
-					$sender->sendMessage("----------Commands For Tell Raw----------\n/raw tell - Sends a raw message to a player.\n------------------------------------------");
+				if(empty($args[0])) {
+					$sender->sendMessage("-----------------------------------------\nCommands For iRaw: (Page 1/1)\n- /raw tell <player> <message>\n/raw say <message>\n/raw tell - Tells a player a raw message \n/raw say - Sends a raw message to server\n-----------------------------------------");
 				}
 					if($args[0] == "tell") {
 						$sent = $this->getServer()->getPlayerExact($args[1]);
-						if($invited->isOnline() == true) {
+						$message = $this->getServer()->getMessage($args[2]);
+						if($sent->isOnline() == true) {
 						$message = $array["message"];
 						$sender-> sendMessage("------------------------------------------\n" $message "\nWas successfully sent to $sent\nYour name is not shown Unless you've entered\nYour name in the Message!\n------------------------------------------");
-						$args[1]->sendMessage("$message");
+						$args[1]->getPlayerExact->sendMessage("$message");
 							return true;
 						}
 						if(!$args[1] instanceof Player) {
-							$sender->sendMessage("$invited is not online.");
+							$sender->sendMessage("iRaw] $sent is not online.");
 							return true;
+						}
+						if(!$args[1] = 0) {
+							$sender->sendMessage("iRaw] You Must Enter A Message!");
+							$sender->sendMessage("Usage: /raw tell <player> <message>")
 						} else {
-							$sender->sendMessage("Player not online!");
+							$sender->sendMessage("iRaw] Usage: /raw tell <player> <message>");
 						}
 					}
-}
+					if(args[0] == "say") {
+						$message = $this->getServer()->getMessage($args[1]);
+		                $sender->getServer()->broadcastMessage($message);
+					}
+						if(!$args[1] = 0) {
+							$sender->sendMessage("iRaw] You Must Enter A Message!");
+							$sender->sendMessage("Usage: /raw say <message>")
+					} else {
+						$sender->sendMessage("iRaw] Usage: /raw say <message>");
+					}
+						   
+		}
